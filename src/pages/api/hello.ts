a78e34c2b5e55166d2cf39311ea0307238a8966c
jeffrey <jeffrey@infloww.com>
2025-05-08 19:26:28 +0800

    1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,5 @@
-export const runtime = 'edge';
+// export const runtime = 'edge';
+export const config = { runtime: 'edge' };
 
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
